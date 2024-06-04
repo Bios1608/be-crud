213d17c36e0736e48adaea59dcb9319115faae5e
Bios1608 <120409886+Bios1608@users.noreply.github.com>
2024-06-04 22:01:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 8080
 const jwt = require('jsonwebtoken');
 const secretKey = 'your_secret_key'; // Replace with your secret key
 const multer = require('multer');
